3e95a5ffe379c5b1cfa5eedda6d0354553e4a72d
NoamYes <noamiko600@gmail.com>
2020-12-09 22:10:10 +0200

    Succesful laplacian

--- Q3/main.py
+++ Q3/main.py
@@ -17,27 +17,19 @@ rn_edges = np.append(rn_edges, [[1,n]], axis=0)
 pn_adj_mat = np.zeros((n,n))
 rn_adj_mat = np.zeros((n,n))
 for edge in pn_edges:
-    pn_adj_mat[edge[0],edge[1]] = 1 
+    pn_adj_mat[edge[0]-1,edge[1]-1] = 1 
+    pn_adj_mat[edge[1]-1,edge[0]-1] = 1 
 for edge in rn_edges:
-    rn_adj_mat[edge[0],edge[1]] = 1    
-     
-# pn_adj_mat = np.diag(np.ones(n-1), k=-1) + np.diag(np.ones(n-1), k=1)
-
-# rn_adj_mat = pn_adj_mat
-# rn_adj_mat[0,n-1] = 1
-# rn_adj_mat[n-1,0] = 1
+    rn_adj_mat[edge[0]-1,edge[1]-1] = 1 
+    rn_adj_mat[edge[1]-1,edge[0]-1] = 1   
 
 # Compute degree matrices 
-pn_deg_mat = np.sum(pn_adj_mat)
-rn_deg_mat = np.sum(rn_adj_mat)
+pn_deg_mat = np.diag(np.sum(pn_adj_mat, axis=0))
+rn_deg_mat = np.diag(np.sum(rn_adj_mat, axis=0))
+
+# Compute Laplacian matrices 
+pn_lap_mat = pn_deg_mat - pn_adj_mat
+rn_lap_mat = rn_deg_mat - rn_adj_mat
 
 
-Pn_graph = nx.path_graph(n)
-Rn_graph = nx.cycle_graph(n)
-
-edge = Pn_graph.edges
-Pn_nodes = Pn_graph.nodes(2)
-Pn_edges = Pn_graph.edges(1)
-
-pn_adj_mat = np.array(Pn_graph.adjacency())
 print('yaa wei eileen lee')
